86f9bc32f8a69d462df2b41c8030c094b3def353
jaejinee <012h21l@gmail.com>
2025-02-13 18:59:55 +0900

    apple shop day1 250213

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,96 @@
 import "./App.css";
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { useState } from "react";
+import data from "./data.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/detail.jsx";
 
 function App() {
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
   return (
     <div className="App">
-      <Navbar className="bg-body-tertiary">
+      <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              Signed in as: <a href="#login">Mark Otto</a>
-            </Navbar.Text>
-          </Navbar.Collapse>
+          <Navbar.Brand href="#home">Shoe Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Details
+            </Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
 
-      <div
-        className="main-bg"
-        style={{
-          backgroundImage:
-            "url(https://www.w3schools.com/w3images/forestbridge.jpg)",
-        }}
-      >
-        {" "}
-      </div>
-
-      <Container>
-        <Row>
-          <Col sm>
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              alt=""
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col sm>
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              alt=""
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col sm>
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              alt=""
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-        </Row>
-      </Container>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"> </div>
+              <Container>
+                <Row>
+                  {shoes.map((shoe, i) => {
+                    return <Card shoes={shoe} i={i} />;
+                  })}
+                </Row>
+              </Container>
+            </>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={"첫 주문시 양배추즙 서비스"} />
+          <Route path="two" element={"생일기념 쿠폰받기"} />
+        </Route>
+        <Route path="*" element={<div>없는 페이지</div>} />
+      </Routes>
     </div>
   );
 }
 
+function Card(props) {
+  // let [shoesImg] = useState([
+  //   "https://codingapple1.github.io/shop/shoes1.jpg",
+  //   "https://codingapple1.github.io/shop/shoes2.jpg",
+  //   "https://codingapple1.github.io/shop/shoes3.jpg",
+  // ]);
+  return (
+    <Col sm>
+      {/* <img src={shoesImg[props.i]} alt="" width="80%" /> */}
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        alt=""
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </Col>
+  );
+}
+
+function Event() {
+  return (
+    <>
+      <div>오늘의 이벤트</div>
+      <Outlet />
+    </>
+  );
+}
 export default App;
